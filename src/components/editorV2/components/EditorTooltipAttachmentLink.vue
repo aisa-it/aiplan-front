9b7ea01555b31ac4275a4e7ba124929e1ac9ee0e
Ефимов Алексей Витальевич <aleksey.efimov@aisa.ru>
2025-09-12 10:52:04 +0300

    feat: загрузка файла по клику на ссылку вложения в редакторе FRO-722

--- src/components/editorV2/components/EditorTooltipAttachmentLink.vue
+++ src/components/editorV2/components/EditorTooltipAttachmentLink.vue
@@ -7,11 +7,13 @@
     <a
       v-if="!loading"
       class="flex items-center gap-x-xs"
-      :href="node.attrs.originalUrl"
-      target="_blank"
+      :href="props.node.attrs?.originalUrl"
+      @click.prevent="
+        handleDownload(props.node.attrs?.originalUrl, props.node.attrs?.title)
+      "
       style="text-decoration: none; color: inherit"
     >
-      <span class="link-like">{{ props.node.attrs.title }}</span>
+      <span class="link-like">{{ props.node.attrs?.title }}</span>
 
       <HintTooltip v-if="sourse">
         {{ tooltip }}
@@ -22,6 +24,7 @@
 </template>
 
 <script setup lang="ts">
+import axios from 'axios';
 import { computed, onMounted, ref } from 'vue';
 import { nodeViewProps, NodeViewWrapper } from '@tiptap/vue-3';
 import { useLoad } from 'src/composables/useLoad';
@@ -33,21 +36,41 @@ const aidocStore = useAiDocStore();
 const sourse = ref();
 
 const tooltip = computed(() => {
-  const title = props.node.attrs.title;
-  const type = props.node.attrs.type === 'aidoc' ? 'документу' : 'задаче';
+  const title = props.node.attrs?.title;
+  const type = props.node.attrs?.type === 'aidoc' ? 'документу' : 'задаче';
   return `${title} прикреплён к ${type} ${sourse.value}`;
 });
 
 const getAttachmentData = async () => {
   if (props.node.attrs.type === 'aidoc') {
     sourse.value = (
-      await aidocStore.getAiDoc(props.node.attrs.slug, props.node.attrs.docId)
+      await aidocStore.getAiDoc(props.node.attrs?.slug, props.node.attrs?.docId)
     )?.data?.title;
   } else {
     sourse.value =
       props.node.attrs?.projectIdentifier +
       '-' +
       props.node.attrs?.currentIssueId;
+  }
+};
+
+const handleDownload = async (link: string, filename: string) => {
+  const { data } = await axios.get(link, {
+    responseType: 'blob',
+    withCredentials: true,
+  });
+
+  const blob = new Blob([data]);
+  const url = URL.createObjectURL(blob);
+  try {
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = filename;
+    document.body.appendChild(a);
+    a.click();
+    a.remove();
+  } finally {
+    URL.revokeObjectURL(url);
   }
 };
 
