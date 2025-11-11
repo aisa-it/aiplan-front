40f667fb576d856e6866feabe884731683b387f7
mitory <dmitriy.zheleznev@aisa.ru>
2025-11-11 11:25:12 +0300

    feat: рефактор загаловка задач и обновление либ FRO-648

--- src/pages/SprintsPage.vue
+++ src/pages/SprintsPage.vue
@@ -1,51 +1,84 @@
 <template>
   <q-page>
-    <p>Здесь будет страница с задачами</p>
+    <q-card class="single-list relative" flat dense>
+      <q-card-section
+        class="row issue-list__header"
+        :style="'padding: 12px 16px'"
+      >
+        <div
+          class="q-table__title abbriviated-text"
+          :style="' max-width: calc(100% - 60px)'"
+        >
+          <span> Задачи спринта {{ sprint?.name }} </span>
+        </div>
+        <q-space />
+      </q-card-section>
+      <q-separator />
+      <transition name="fade" mode="out-in">
+        <component :is="currentIssueList" />
+      </transition>
+    </q-card>
   </q-page>
 </template>
 
 <script lang="ts" setup>
-// v-show="!isLoadProjectInfo"
 import { useRouter } from 'vue-router';
-import { onMounted } from 'vue';
+import {
+  defineAsyncComponent,
+  onMounted,
+  ref,
+  shallowRef,
+  watchEffect,
+} from 'vue';
 
 import { getSprint } from 'src/modules/sprints/services/api';
-
-// import { storeToRefs } from 'pinia';
-// import { useProjectStore } from 'src/stores/project-store';
-// import { useWorkspaceStore } from 'src/stores/workspace-store';
-// import { useViewPropsStore } from 'src/stores/view-props-store';
-
-// import { getSprints, createSprint } from 'src/modules/sprints/services/api';
+import { DtoSprint } from '@aisa-it/aiplan-api-ts/src/data-contracts';
 
 const router = useRouter();
-// const projectStore = useProjectStore();
-// const workspaceStore = useWorkspaceStore();
-// const viewProps = useViewPropsStore();
-// const { currentProjectID, isLoadProjectInfo } = storeToRefs(projectStore);
-// const { currentWorkspaceSlug } = storeToRefs(workspaceStore);
+const sprint = ref({} as DtoSprint);
+const issuesLoader = ref(false);
 
-// const getCurrentProject = async () => {
-//   isLoadProjectInfo.value = true;
-//   currentProjectID.value = 'LED';
+const components = {
+  DefaultIssueList: defineAsyncComponent(
+    () => import('src/modules/issue-list/components/DefaultIssueList.vue'),
+  ),
+  GroupedIssueList: defineAsyncComponent(
+    () => import('src/modules/issue-list/components/GroupedIssueList.vue'),
+  ),
+  TableListSkeleton: defineAsyncComponent(
+    () =>
+      import(
+        'src/modules/issue-list/components/skeletons/TableListSkeleton.vue'
+      ),
+  ),
+  BoardListSkeleton: defineAsyncComponent(
+    () =>
+      import(
+        'src/modules/issue-list/components/skeletons/BoardListSkeleton.vue'
+      ),
+  ),
+};
 
-//   await projectStore.getProjectInfo(
-//     router.currentRoute.value.params.workspace as string,
-//     'LED',
-//   );
-//   await projectStore.getMeInProject(
-//     router.currentRoute.value.params.workspace as string,
-//     'LED',
-//   );
-//   await viewProps.getProjectProps();
-//   isLoadProjectInfo.value = false;
-// };
+const currentIssueList = shallowRef();
+
+watchEffect(() => {
+  if (issuesLoader.value === false) {
+    currentIssueList.value = false //группа
+      ? components.GroupedIssueList
+      : components.DefaultIssueList;
+  } else {
+    currentIssueList.value = false //канбан
+      ? components.BoardListSkeleton
+      : components.TableListSkeleton;
+  }
+});
 
 onMounted(async () => {
-  const res = await getSprint(
+  issuesLoader.value = true;
+  sprint.value = await getSprint(
     router.currentRoute.value.params.workspace as string,
     router.currentRoute.value.params.sprint as string,
   );
-  console.log(res);
+  issuesLoader.value = false;
 });
 </script>
