7521dc63093df204859653eccf27b69d288ee748
Viktor <viktor.afanasev@aisa.ru>
2025-12-15 11:13:14 +0300

    feat: Обновлены гирлянды и осадки в новогодней теме FRO-784

--- src/components/LightsNewYear.vue
+++ src/components/LightsNewYear.vue
@@ -248,30 +248,23 @@ body {
     border-radius: 50%;
     margin: calc($globe-spacing/2);
     display: inline-block;
-    background: rgb(136, 255, 132, 1);
+    background: #00f7a5;
     box-shadow: 0px calc($globe-height/6) $globe-width * 2 $globe-spread
-      rgba(136, 255, 132, 0.5);
+    #00f7a5;
     animation-name: flash-1;
     animation-duration: 4s;
     &:nth-child(2n + 1) {
-      background: rgb(84, 115, 255);
+      background: cyan;
       box-shadow: 0px calc($globe-height/6) $globe-width * 2 $globe-spread
-        rgba(84, 115, 255, 0.5);
+        cyan;
       animation-name: flash-2;
       animation-duration: 2.4s;
     }
     &:nth-child(4n + 1) {
-      background: rgb(255, 96, 96);
+      background: #f70094;
       box-shadow: 0px calc($globe-height/6) $globe-width * 2 $globe-spread
-        rgba(255, 96, 96, 1);
+        #f70094;
       animation-name: flash-3;
-      animation-duration: 3.1s;
-    }
-    &:nth-child(4n + 2) {
-      background: rgb(252, 249, 102);
-      box-shadow: 0px calc($globe-height/6) $globe-width * 2 $globe-spread
-        rgba(252, 249, 102, 1);
-      animation-name: flash-4;
       animation-duration: 3.1s;
     }
     &:nth-child(odd) {
@@ -311,54 +304,40 @@ body {
 @keyframes flash-1 {
   0%,
   100% {
-    background: rgb(136, 255, 132);
+    background: #00f7a5;
     box-shadow: 0px calc($globe-height/6) $globe-width * 2 $globe-spread
-      rgba(136, 255, 132, 1);
+    rgba(0, 247, 165, 0.2);
   }
   50% {
-    background: rgba(102, 255, 97, $light-off-opacity);
+    background: rgba(0, 247, 165, $light-off-opacity);
     box-shadow: 0px calc($globe-height/6) $globe-width * 2 $globe-spread
-      rgba(94, 255, 88, 0.2);
+    rgba(0, 247, 165, 0.2);
   }
 }
 @keyframes flash-2 {
   0%,
   100% {
-    background: rgba(84, 115, 255, 1);
+    background: cyan;
     box-shadow: 0px calc($globe-height/6) $globe-width * 2 $globe-spread
-      rgba(84, 115, 255, 1);
+    cyan;
   }
   50% {
-    background: rgba(84, 115, 255, $light-off-opacity);
+    background: rgba(0, 255, 255, $light-off-opacity);
     box-shadow: 0px calc($globe-height/6) $globe-width * 2 $globe-spread
-      rgba(84, 115, 255, 0.2);
+    rgba(0, 255, 255, 0.2);
   }
 }
 @keyframes flash-3 {
   0%,
   100% {
-    background: rgba(255, 96, 96, 1);
+    background: #f70094;
     box-shadow: 0px calc($globe-height/6) $globe-width * 2 $globe-spread
-      rgba(255, 96, 96, 1);
+    #f70094;
   }
   50% {
-    background: rgba(255, 96, 96, $light-off-opacity);
+    background: rgba(247, 0, 148, $light-off-opacity);
     box-shadow: 0px calc($globe-height/6) $globe-width * 2 $globe-spread
-      rgba(255, 96, 96, 0.2);
-  }
-}
-
-@keyframes flash-4 {
-  0%,
-  100% {
-    background: rgba(252, 249, 102, 1);
-    box-shadow: 0px calc($globe-height/6) $globe-width * 2 $globe-spread
-      rgba(252, 249, 102, 1);
-  }
-  50% {
-    background: rgba(252, 249, 102, $light-off-opacity);
-    box-shadow: 0px calc($globe-height/6) $globe-width * 2 $globe-spread
-      rgba(252, 249, 102, 0.2);
+    rgba(247, 0, 148, 0.2);
   }
 }
 </style>
