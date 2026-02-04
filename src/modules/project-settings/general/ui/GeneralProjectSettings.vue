1ca1a0bc3f16262de25d0f9ccc3fbb4cd3137cea
Валерия Шашкова <valeriia.shashkova@aisa.ru>
2026-02-04 15:11:44 +0300

    feat: увеличена валидная длина идентификатора проекта до 15 символов

--- src/modules/project-settings/general/ui/GeneralProjectSettings.vue
+++ src/modules/project-settings/general/ui/GeneralProjectSettings.vue
@@ -186,6 +186,7 @@ import { getUrlFile } from 'src/utils/helpers';
 // constants
 import { NETWORK_CHOICES } from 'src/constants/constants';
 import { PROJECT_EMOJI_OPTIONS } from 'src/constants/emojis';
+import { PROJECT_IDENTIFIER_LENGTH  } from 'src/constants/constants';
 
 // interfaces
 import { IProject } from 'src/interfaces/projects';
@@ -294,9 +295,9 @@ const validateName = (val: string): boolean | string => {
 const validateIdentifier = (val: string): boolean | string => {
   isValidIdentifier.value = false;
   const minL =
-    val.trim().length >= 3 ||
-    'Идентификатор должен содержать 3 и более симолов';
-  const maxL = maxLength(val, 10);
+    val.trim().length >= PROJECT_IDENTIFIER_LENGTH.MIN ||
+    `Идентификатор должен содержать ${PROJECT_IDENTIFIER_LENGTH.MIN} и более симолов`;
+  const maxL = maxLength(val, PROJECT_IDENTIFIER_LENGTH.MAX);
   const upperCaseAndNumber = isUpperCaseAndNumber(val, 'Идентификатор');
 
   if (typeof minL === 'string') {
