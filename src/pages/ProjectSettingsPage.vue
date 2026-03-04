a4bf662cc41c1438e45d10a897871bb2ec7efd40
Валерия Шашкова <valeriia.shashkova@aisa.ru>
2026-03-04 13:30:50 +0300

    feat: добавлено подтверждение закрытия редактирования шаблона

--- src/pages/ProjectSettingsPage.vue
+++ src/pages/ProjectSettingsPage.vue
@@ -8,7 +8,7 @@
       <SettingsTabs
         :current-tab="projectSettingsTab"
         :list-tabs="listTabs"
-        @set="(val: number) => (projectSettingsTab = val)"
+        @set="handleSet"
       />
 
       <div
@@ -19,7 +19,14 @@
         <DefaultLoader class="self-center q-mt-md q-mb-md" />
       </div>
 
-      <component v-else :is="activeTab" @update="projectInfoRefresh" />
+      <component
+        v-else
+        :is="activeTab"
+        :open-dialog="isLeavingTabRestricted ? showDialog : false"
+        @update="projectInfoRefresh"
+        @set-restriction="handleRestrictionUpdate"
+        @back-to-edition="resetTabRouting"
+      />
     </div>
   </q-page>
 </template>
@@ -51,7 +58,7 @@ const { roles } = storeToRefs(useRolesStore());
 const { project } = storeToRefs(projectStore);
 
 // tabs
-const projectSettingsTab = ref(0);
+const projectSettingsTab = ref(7);
 
 const route = useRoute();
 
@@ -187,6 +194,9 @@ const listTabs = computed(() => {
   });
 });
 
+const isLeavingTabRestricted = ref<boolean>(false);
+const showDialog = ref<boolean>(false);
+
 useMeta(() => {
   return {
     title: metadata.value.title,
@@ -209,6 +219,41 @@ const projectInfoRefresh = async () => {
         window.location.href = '/access-denied';
       }
     });
+};
+
+const pendingTab = ref<number | null>();
+const handleSet = (val: number) => {
+  if (projectSettingsTab.value === 7) {
+    if (isLeavingTabRestricted.value) {
+      showDialog.value = true;
+      pendingTab.value = val;
+      return;
+    } else {
+      projectSettingsTab.value = val;
+      if (pendingTab.value) {
+        pendingTab.value = null;
+      }
+    }
+  } else {
+    projectSettingsTab.value = val;
+  }
+};
+
+const handleRestrictionUpdate = (isRestricted: boolean) => {
+  isLeavingTabRestricted.value = isRestricted;
+  if (!isRestricted) {
+    showDialog.value = false;
+    if (pendingTab.value) {
+      handleSet(pendingTab.value);
+    }
+  }
+};
+
+const resetTabRouting = () => {
+  showDialog.value = false;
+  if (pendingTab.value) {
+    pendingTab.value = null;
+  }
 };
 
 onMounted(() => {
