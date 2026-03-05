b4e75a1fb619c9760806600ad1e40327a0c41446
mitory <101456279+mitory@users.noreply.github.com>
2026-03-05 16:09:31 +0300

    feat: добавлена тема на 8 марта FRO-862 (#380)
    * feat: добавлена тема на 8 марта FRO-862
    
    * feat: тема 8 марта активируется с 5 по 9 марта FRO-862

---
+++ src/components/FlowerLine.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="flower-line" />
+</template>
+
+<style lang="scss" scoped>
+.flower-line {
+  width: 100%;
+  height: 40px;
+  z-index: 101;
+  pointer-events: none;
+
+  background-image: url('src/assets/patterns/pattern_flower.svg');
+  background-repeat: repeat-x;
+  background-position: top left;
+  background-size: 174.88px 40px;
+  background-color: $bg-color;
+}
+</style>
--- src/components/search-panel/SearchPanel.vue
+++ src/components/search-panel/SearchPanel.vue
@@ -4,7 +4,13 @@
       v-if="ny"
       fit="contain"
       :src="newYearTree"
-      style="width: 38px; height: 32px;"
+      style="width: 38px; height: 32px"
+    />
+    <q-img
+      v-if="wd && !q.platform.is.mobile"
+      fit="contain"
+      :src="womanDayText"
+      style="width: 83px; height: 32px"
     />
     <q-btn
       v-if="isEnabledJitsi"
@@ -26,6 +32,7 @@
 <script setup lang="ts">
 // core
 import { useRoute } from 'vue-router';
+import { useQuasar } from 'quasar';
 
 // components
 import ProfileButton from 'src/components/ProfileButton.vue';
@@ -37,14 +44,17 @@ import { storeToRefs } from 'pinia';
 import { useUtilsStore } from 'src/stores/utils-store';
 //icons
 import newYearTree from 'src/assets/newYearTree.svg';
+import womanDayText from 'src/assets/woman-day-text.png';
 
 const route = useRoute();
 
 const utilsStore = useUtilsStore();
 
+const q = useQuasar();
+
 //storesToRefs
 const { isEnabledJitsi } = storeToRefs(utilsStore);
-const { ny } = storeToRefs(utilsStore);
+const { ny, wd } = storeToRefs(utilsStore);
 
 function openConference() {
   window.open(`/conf/`);
