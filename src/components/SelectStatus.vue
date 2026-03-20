07d1a9f8540b0f95be9ec023621a29f879f864ec
Viktor Afanasev <viktor.afanasev@aisa.ru>
2026-03-20 16:07:55 +0300

    fix: доработка по ограничению статусов задач FRO-870

--- src/components/SelectStatus.vue
+++ src/components/SelectStatus.vue
@@ -206,6 +206,7 @@ export default defineComponent({
             },
           )
           .then(() => {
+            refresh()
             useSprintStore().triggerSprintRefresh(NotUpdated.SprintPage);
             showNotification('success');
             emit('setStatus', state);
