52c8eef10f979ef0e8dcc9b458285f7f20672553
Валерия Шашкова <valeriia.shashkova@aisa.ru>
2026-04-10 15:44:10 +0300

    feat: удалены неиспользуемые свойства и методы

--- src/stores/states-store.ts
+++ src/stores/states-store.ts
@@ -1,45 +1,18 @@
 import { defineStore } from 'pinia';
-import { useAiplanStore } from './aiplan-store';
-import { getStatesList } from 'src/utils/helpers';
 import { withInterceptors } from 'src/utils/interceptorsWithInstanceClass';
 import { Projects } from '@aisa-it/aiplan-api-ts/src/Projects';
 
 const statesApi = new (withInterceptors(Projects))();
-const aiplan = useAiplanStore();
 
 export const useStatesStore = defineStore('states', {
   state: () => {
     return {
-      projectId: '',
-      states: {},
       statesCache: {},
-      draggingState: '' as string,
     };
   },
   actions: {
-    async getStates(projectId: string) {
-      this.projectId = projectId;
-      return await this.refresh();
-    },
-    async refresh() {
-      if (this.projectId === '') {
-        return;
-      }
-      return await aiplan.getStates(this.projectId).then(({ data }) => {
-        this.states = data;
-        return this.states;
-      });
-    },
-    async getStatesList(projectId: string, onlyActive: boolean) {
-      return this.getStates(projectId).then(() =>
-        getStatesList(this.states, onlyActive),
-      );
-    },
     async getStatesByProject(workspaceSLug: string, projectID: string) {
       return statesApi.getStateList(workspaceSLug,projectID)
     },
-    setDraggingState(state: string) {
-      this.draggingState = state
-    }
   },
 });
