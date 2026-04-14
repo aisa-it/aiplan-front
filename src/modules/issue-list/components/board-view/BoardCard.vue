5cc99b4b71a3be4784ea37c344b12829618ad8d9
mitory <101456279+mitory@users.noreply.github.com>
2026-04-14 09:05:23 +0300

    fix: открытие родительской задачи в спринтах BUGS-1210 (#432)
    * fix: открытие родительской задачи в спринтах BUGS-1210
    
    * fix: убран доп параметр для превью родительской задачи BUGS-1210

--- src/modules/issue-list/components/board-view/BoardCard.vue
+++ src/modules/issue-list/components/board-view/BoardCard.vue
@@ -35,7 +35,13 @@
         :row="card"
         :target="user.theme?.open_in_new ? '_blank' : '_self'"
         class="parent-issue-chip"
-        @click.prevent.stop="emits('openPreview', card.parent_detail)"
+        @click.prevent.stop="
+          emits('openPreview', {
+            ...card.parent_detail,
+            project_detail: { identifier: card.project_detail.identifier },
+            project: card.project,
+          })
+        "
       />
     </div>
 
