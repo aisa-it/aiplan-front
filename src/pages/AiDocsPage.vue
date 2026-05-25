b2b6709cc13b3245b1c3a656e5dad6dd6fb14cc5
Viktor Afanasev <viktor.afanasev@aisa.ru>
2026-05-25 16:57:45 +0300

    fix: правки по боковому меню страница форм и аидок FRO-887

---
+++ src/pages/AiDocsPage.vue
@@ -0,0 +1,10 @@
+<template>
+  <q-page class="q-pa-md">
+    <h4>АИДок</h4>
+    <AidocTable />
+  </q-page>
+</template>
+
+<script setup lang="ts">
+import AidocTable from 'src/modules/aidoc-table/components/AidocTable.vue';
+</script>
