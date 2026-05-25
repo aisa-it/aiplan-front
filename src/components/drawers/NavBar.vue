a18db0365ad8957028bae4c9d8d1511bd92106cd
Viktor Afanasev <viktor.afanasev@aisa.ru>
2026-05-25 13:17:10 +0300

    feat: доработка страницы форм и аидок FRO-887

--- src/components/drawers/NavBar.vue
+++ src/components/drawers/NavBar.vue
@@ -8,7 +8,7 @@
     bordered
     class="main-nav-bar"
   >
-    <q-scroll-area class="fit" :horizontal-thumb-style="{ opacity: 0 }">
+    <q-scroll-area class="fit" :horizontal-thumb-style="{ opacity: '0' }">
       <q-list padding>
         <q-item
           clickable
@@ -54,7 +54,12 @@
           <q-item-section> Спринты </q-item-section>
         </q-item>
 
-        <q-item clickable v-ripple>
+        <q-item
+          :active="route.path.includes('/forms')"
+          clickable
+          v-ripple
+          @click="router.push(`/${currentWorkspaceSlug}/forms`)"
+        >
           <q-item-section avatar>
             <MenuFormsIcon :is-dark="$q.dark.isActive" />
           </q-item-section>
@@ -62,7 +67,12 @@
           <q-item-section> Формы </q-item-section>
         </q-item>
 
-        <q-item clickable v-ripple>
+        <q-item
+          :active="route.path.includes('/aidoc')"
+          clickable
+          v-ripple
+          @click="router.push(`/${currentWorkspaceSlug}/aidoc`)"
+        >
           <q-item-section avatar>
             <AIDocIcon />
           </q-item-section>
