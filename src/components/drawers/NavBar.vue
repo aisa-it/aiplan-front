df46460a50c7b47c8c0680f671f16fd42aec0133
Валерия Шашкова <valeriia.shashkova@aisa.ru>
2026-06-17 14:04:01 +0300

    Revert "Merge branch 'dev' of https://github.com/aisa-it/aiplan-front into dev"
    This reverts commit fbb638556aa23b67e93914c5897720176c3e53fc, reversing
    changes made to 2ea65258224fea2c1cab67e44084814d19b7eda1.

--- src/components/drawers/NavBar.vue
+++ src/components/drawers/NavBar.vue
@@ -3,15 +3,11 @@
     v-model="drawer"
     show-if-above
     :mini="!drawer || miniState"
-    :width="isMobile ? DEFAULT_WIDTH : adaptiveWidth"
+    :width="270"
     :breakpoint="500"
     bordered
     class="main-nav-bar"
-    @mouseenter="isBtnShow = true"
-    @mouseleave="isBtnShow = false"
-    @before-show="updateClientWidth"
-    @mini-state="saveState"
-    >
+  >
     <div class="column full-height">
       <q-scroll-area class="col" :horizontal-thumb-style="{ opacity: 0 }">
         <q-list>
@@ -25,27 +21,27 @@
             }"
           >
             <q-item-section avatar>
-              <HomeIcon :color="route.name === 'workspace' ? ACTIVE_ICON_COLOR : ''"/>
+              <HomeIcon />
             </q-item-section>
 
             <q-item-section> Главная </q-item-section>
           </q-item>
 
           <q-item
-            :active="route.path.includes('projects')"
+            :active="route.name === 'projects'"
             clickable
             v-ripple
             @click="router.push(`/${currentWorkspaceSlug}/projects`)"
           >
             <q-item-section avatar>
-              <MenuProjectsIcon :color="route.path.includes('projects') ? ACTIVE_ICON_COLOR : ''" />
+              <MenuProjectsIcon :is-dark="$q.dark.isActive" />
             </q-item-section>
 
             <q-item-section> Проекты </q-item-section>
           </q-item>
 
           <q-item
-            :active="route.path.includes('sprints')"
+            :active="route.name === 'sprints'"
             clickable
             v-ripple
             :to="{
@@ -53,7 +49,7 @@
             }"
           >
             <q-item-section avatar>
-              <SprintIcon :color="route.path.includes('/sprints') ? ACTIVE_ICON_COLOR : ''" />
+              <SprintIcon />
             </q-item-section>
 
             <q-item-section> Спринты </q-item-section>
@@ -66,7 +62,7 @@
             @click="router.push(`/${currentWorkspaceSlug}/forms`)"
           >
             <q-item-section avatar>
-              <MenuFormsIcon :color="route.path.includes('/forms') ? ACTIVE_ICON_COLOR : ''" />
+              <MenuFormsIcon :is-dark="$q.dark.isActive" />
             </q-item-section>
 
             <q-item-section> Формы </q-item-section>
@@ -79,7 +75,7 @@
             @click="router.push(`/${currentWorkspaceSlug}/aidoc`)"
           >
             <q-item-section avatar>
-              <AIDocIcon :color="route.path.includes('/aidoc') ? ACTIVE_ICON_COLOR : ''" />
+              <AIDocIcon />
             </q-item-section>
 
             <q-item-section> АИДок </q-item-section>
@@ -124,10 +120,9 @@
 
     <div class="absolute" style="top: 15px; right: -10px">
       <q-btn
-        v-show="isBtnShow"
         class="drawer-btn"
         :icon="miniState ? 'chevron_right' : 'chevron_left'"
-        @click="onDrawerBtnClick"
+        @click="drawerClick"
       />
     </div>
 
@@ -137,20 +132,18 @@
       @success="(msg) => onSuccess(msg)"
     />
     <ReleaseNotePreviewDialog v-model="isReleaseOpen" />
-    <div v-show="!miniState" class="handle-resize" @pointerdown="onPointerDown"></div>
   </q-drawer>
 </template>
 
 <script setup lang="ts">
 // core
-import { computed, onBeforeMount, onUnmounted, ref } from 'vue';
-import { Screen, useQuasar } from 'quasar';
+import { ref } from 'vue';
+import { useQuasar } from 'quasar';
 import { storeToRefs } from 'pinia';
 import { useRoute, useRouter } from 'vue-router';
 
 // services
 import { useNotificationStore } from 'src/stores/notification-store';
-import { useWorkspaceStore } from 'src/stores/workspace-store';
 import { SUCCESS_UPDATE_DATA } from 'src/constants/notifications';
 
 // icons
@@ -167,12 +160,12 @@ import AiplanHelpDialog from 'src/components/dialogs/AiplanHelp/AiplanHelpDialog
 import ReleaseNotePreviewDialog from 'components/dialogs/ReleaseNotePreviewDialog.vue';
 import ExpansionItem from '../ExpansionItem.vue';
 import NavBarHelpList from './NavBarHelpList.vue';
-import { useDrawerResize } from 'src/composables/useDrawerResize';
+
+import { useWorkspaceStore } from 'src/stores/workspace-store';
 
 const $q = useQuasar();
 const miniState = ref(false);
 const drawer = ref(true);
-
 const router = useRouter();
 const route = useRoute();
 
@@ -181,43 +174,8 @@ const { setNotificationView } = useNotificationStore();
 const isHelpOpen = ref(false);
 const isFeedbackOpen = ref(false);
 const isReleaseOpen = ref(false);
-const isBtnShow = ref(false);
 
-const ACTIVE_ICON_COLOR = '#3f75ff';
-const STORAGE_KEY = 'isMiniState';
-const DEFAULT_WIDTH = 270;
-
-const clientWidth = ref(document.documentElement.clientWidth);
-const isMobile = computed(() => {
-  return $q.platform.is.mobile && Screen.lt.md;
-});
-const minWidth = computed(() => DEFAULT_WIDTH);
-const maxWidth = computed(() =>
-  isMobile.value ? DEFAULT_WIDTH : clientWidth.value / 2,
-);
-const { adaptiveWidth, onPointerDown, updateClientWidth } = useDrawerResize(
-  minWidth,
-  maxWidth,
-  clientWidth,
-  'menuSidebarWidth',
-  'left',
-);
-
-const onDrawerBtnClick = () => (miniState.value = !miniState.value);
-
-const syncDrawerFromStorage = (e: StorageEvent) => {
-  if (e.key === STORAGE_KEY) {
-    try {
-      miniState.value = JSON.parse(e.newValue as string);
-    } catch {
-      miniState.value = false;
-    }
-  }
-};
-
-const saveState = () => {
-  localStorage.setItem(STORAGE_KEY, String(miniState.value));
-}
+const drawerClick = () => (miniState.value = !miniState.value);
 
 const onSuccess = (msg?: string) => {
   setNotificationView({
@@ -228,31 +186,4 @@ const onSuccess = (msg?: string) => {
 };
 
 const { currentWorkspaceSlug } = storeToRefs(useWorkspaceStore());
-
-onBeforeMount(() => {
-  const stored = localStorage.getItem(STORAGE_KEY);
-  miniState.value = stored ? JSON.parse(stored) : true;
-  window.addEventListener('storage', syncDrawerFromStorage);
-});
-
-onUnmounted(() => {
-  window.removeEventListener('storage', syncDrawerFromStorage);
-});
 </script>
-
-<style scoped>
-.handle-resize {
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  right: 0;
-  width: 6px;
-  cursor: col-resize;
-  user-select: none;
-  touch-action: none;
-}
-
-.drawer-btn {
-  z-index: 1000;
-}
-</style>
--- src/components/icons/SprintIcon.vue
+++ src/components/icons/SprintIcon.vue
@@ -4,7 +4,6 @@
     height="24"
     viewBox="0 0 24 24"
     fill="none"
-    :class="color ? '' : 'default-icon-color'"
     xmlns="http://www.w3.org/2000/svg"
   >
     <path
