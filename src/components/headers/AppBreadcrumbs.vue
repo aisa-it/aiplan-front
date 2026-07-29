c7c81c6c796bb9e077daa21d48102cdcd47fbd67
Viktor Afanasev <viktor.afanasev@aisa.ru>
2026-07-29 14:46:33 +0300

    feat: добавлена логика и отображение breadcrumbs vuetify FRO-910

---
+++ src/components/headers/AppBreadcrumbs.vue
@@ -0,0 +1,39 @@
+<template>
+  <nav
+    v-if="items.length"
+    class="ml-4 flex min-w-0 items-center overflow-x-auto text-[14px] font-extrabold text-[#474a52]"
+  >
+    <template v-for="(item, index) in items" :key="`${item.title}-${index}`">
+      <span v-if="index > 0" class="mx-2 shrink-0 select-none">/</span>
+
+      <component
+        :is="item.to ? 'router-link' : 'span'"
+        :to="item.to"
+        class="breadcrumbs-title inline-flex min-w-0 items-center gap-1 no-underline"
+        :class="item.to ? 'cursor-pointer text-[#474a52]' : 'text-[#474a52]'"
+      >
+        <HomeIcon
+          v-if="item.icon === 'home'"
+          :width="20"
+          :height="20"
+          class="shrink-0"
+        />
+        <span class="truncate">{{ item.title }}</span>
+      </component>
+    </template>
+  </nav>
+</template>
+
+<script setup lang="ts">
+import { useBreadcrumbs } from '@/composables/useBreadcrumbs'
+
+import HomeIcon from '@/components/icons/HomeIcon.vue'
+
+const { items } = useBreadcrumbs()
+</script>
+
+<style scoped>
+.breadcrumbs-title {
+  max-width: 240px;
+}
+</style>
